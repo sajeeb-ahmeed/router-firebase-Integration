b78544a511c22899393081f7467700df502c7780
sajeeb-ahmeed <sajeeb.web@gmail.com>
2022-04-09 21:18:53 +0600

    Require Auth - Atuthentication

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Login from './components/Login/Login';
 import Orders from './components/Orders/Orders';
 import Products from './components/Products/Products';
 import Register from './components/Register/Register';
+import Require from './components/RequireAuth/Require';
 import Reviews from './components/Reviews/Reviews';
 
 function App() {
@@ -17,7 +18,11 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/home' element={<Home></Home>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
-        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/orders' element={
+          <Require>
+            <Orders></Orders>
+          </Require>
+        }></Route>
         <Route path='/reviews' element={<Reviews></Reviews>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
